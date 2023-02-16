6d773d53900566d0ef06e9ff1ab90d222ca6b3aa
AT-Allen <toluwanielect@gmail.com>
2023-02-16 06:59:38 -0800

    ttt4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - print "Programming is like building a multilingual puzzle"
- * return (0)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
