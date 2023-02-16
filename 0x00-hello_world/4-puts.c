b5f06355a574379a59e79b13312662eb22dabdbc
AT-Allen <toluwanielect@gmail.com>
2023-02-16 06:55:18 -0800

    tt4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,9 @@
 /**
  * main - print "Programming is like building a multilingual puzzle"
  * return (0)
- */ 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-
+			return (0);
 }
